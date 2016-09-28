1e6148eeb5f2e3d32de8cf2b60c1177e0f207cb7
Bobak Hashemi <bth5032@gmail.com>
2016-09-28 13:20:39 -0700

    fixed typo

--- doAll.C
+++ doAll.C
@@ -30,7 +30,7 @@ void runScanChain(ConfigParser* conf){
       ScanChain(getGJetsChain(conf->get("data_set")), "gjets", conf);  
     }
     if (conf->get("ttbar") == "true"){
-      ScanChain(getTTbarMC(conf->get("data_set")), conf->get("data_set"), conf);   
+      ScanChain(getTTBarMC(conf->get("data_set")), conf->get("data_set"), conf);   
     }
   }
 }
