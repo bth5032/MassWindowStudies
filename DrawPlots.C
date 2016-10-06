f222744d48e0927bef192721d923b743c125e054
Bobak <bth5032@gmail.com>
2016-10-06 16:44:22 -0700

    Adding Latex table

--- DrawPlots.C
+++ DrawPlots.C
@@ -645,7 +645,7 @@ tt_met_tight->SetBinContent(tt_met_tight->FindBin(450), bc+of);
 
 	//Write Latex Table----------------------------------------------------------------------------
 	cout<<"Tight Window: "<<endl;
-	cout<<"\\begin{tabular}{ l | l } \hline Sample &"
+	cout<<"\\begin{tabular}{ l | l | l | l | l } \\hline Sample &"
 	for (int i = 1; i<z_met_tight->GetNbinsX()+1; i++){
 		if (i !=z_met_tight->GetNbinsX()){
 			cout<<z_met_tight->GetBinLowEdge(i)<<"-"<<z_met_tight->GetBinLowEdge(i+1)<<" & ";
@@ -654,18 +654,18 @@ tt_met_tight->SetBinContent(tt_met_tight->FindBin(450), bc+of);
 			cout<<z_met_tight->GetBinLowEdge(i)<<"+";	
 		}
 	}
-	cout<<" \\\\ \hline \\\\";
+	cout<<" \\\\ \\hline \\\\";
 	cout<<" ZJets & ";
 	for (int i = 1; i<z_met_tight->GetNbinsX()+1; i++){
 		cout<<z_met_tight->GetBinContent(i)<<" & ";
 	}
-	cout<<" \\\\ \hline \\\\";
+	cout<<" \\\\ \\hline \\\\";
 
 	cout<<" TTbar & ";
 	for (int i = 1; i<tt_met_tight->GetNbinsX()+1; i++){
 		cout<<tt_met_tight->GetBinContent(i)<<" & ";
 	}
-	cout<<" \\\\ \hline \\end{tabular}";
+	cout<<" \\\\ \\hline \\end{tabular}";
 	//Write Latex Table----------------------------------------------------------------------------
 
 
