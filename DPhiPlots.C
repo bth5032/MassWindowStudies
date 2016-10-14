f0ee21dc9e51c46d3147f6f667f82ed2b86de8af
Bobak Hashemi <bth5032@gmail.com>
2016-10-13 23:04:10 -0700

    added v3 plots, ee/mumu dphi met, nvert, njets, genht %-age

--- DPhiPlots.C
+++ DPhiPlots.C
@@ -464,6 +464,9 @@ void DPhiPlots(TString files){
 	TH1D * z_nvert_highphi = (TH1D*) ((TH1D*) z_file->Get("zjets_nVert_highphi"))->Clone("zjets_nVert_highphi");
 	TH1D * z_nvert_lowphi = (TH1D*) ((TH1D*) z_file->Get("zjets_nVert_lowphi"))->Clone("zjets_nVert_lowphi");
 
+	z_nvert_highphi->Scale(z_nvert_highphi->Integral());
+	z_nvert_lowphi->Scale(z_nvert_lowphi->Integral());
+
 	//cout<<__LINE__<<endl;
 
 
@@ -506,6 +509,9 @@ void DPhiPlots(TString files){
 
 	TH1D * z_njets_highphi = (TH1D*) ((TH1D*) z_file->Get("zjets_njets_highphi"))->Clone("z_njets_highphi");
 	TH1D * z_njets_lowphi = (TH1D*) ((TH1D*) z_file->Get("zjets_njets_lowphi"))->Clone("z_njets_lowphi");
+
+	z_njets_highphi->Scale(z_njets_highphi->Integral());
+	z_njets_lowphi->Scale(z_njets_lowphi->Integral());
 
 	//cout<<__LINE__<<endl;
 
