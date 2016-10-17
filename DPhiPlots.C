6c6a23564069c88ecc474840598dff1f99a08429
Bobak Hashemi <bth5032@gmail.com>
2016-10-16 19:29:01 -0700

    added unscaled plot

--- DPhiPlots.C
+++ DPhiPlots.C
@@ -462,43 +462,6 @@ void DPhiPlots(TString files){
 	c_Dht->cd();
 	c_Dht->SaveAs(output_dir+"Z_Dht_met50.png");
 
-		TCanvas *c_Ht_check = new TCanvas("c_Ht_check", "", 2000, 2000);
-
-	gPad->SetLogy(0);
-	gStyle->SetOptStat(kFALSE);
-
-	TH1D * z_genht = (TH1D*) ((TH1D*) z_file->Get("zjets_genht"))->Clone("z_genht");
-	TH1D * z_ht = (TH1D*) ((TH1D*) z_file->Get("zjets_ht"))->Clone("z_ht");
-
-	//cout<<__LINE__<<endl;
-
-
-	z_ht->SetLineColor(9);
-	z_ht->SetLineWidth(6);
-	z_ht->SetTitle("Gen Level H_{T} Vs. Measured H_{T} for Z Events");
-	z_ht->SetAxisRange(0,1000);
-	z_ht->Draw("HIST");
-
-	z_genht->SetLineColor(46);
-	z_genht->SetLineWidth(6);
-	z_genht->Draw("HIST SAME");
-	
-	TLegend *l_ht;
-	l_ht = new TLegend(0.73, 0.73, 0.88, 0.88);
-	  
-	l_ht->SetLineColor(kWhite);  
-	l_ht->SetShadowColor(kWhite);
-	l_ht->SetFillColor(kWhite);
-	l_ht->AddEntry(z_genht, "Gen Level H_{T}", "f");
-	l_ht->AddEntry(z_ht, "Measured H_{T}", "f");
-	  
-	l_ht->Draw("same");
-
-	//Delta HT
-
-	c_Ht_check->cd();
-	c_Ht_check->SaveAs(output_dir+"Ht_check.png");
-
 	TCanvas *c_Dht_unscaled = new TCanvas("c_Dht_unscaled", "", 2000, 2000);
 
 	gPad->SetLogy(0);
